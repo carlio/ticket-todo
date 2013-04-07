4c40cde8a9d78af1cbccd011952da1eb09baa514
carlio <git@carlcrowder.com>
2013-04-07 19:49:10 +0300

    Refactoring tests into unittest and renaming sync module

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
-from sync_issues import parse_issue
+from sync import parse_issue
+from unittest import TestCase
+
+class TestIssueParsing(TestCase):
+
+    def test_completed_issue_with_number(self):
+        status, title, number = parse_issue("+ this is a test issue (512)")
+        self.assertEqual('complete', status)
+        self.assertEqual('this is a test issue', title)
+        self.assertEqual(512, number)
 
 
-def test_issue_parsing():
-    print parse_issue("+ this is a test issue (512)")
-    print parse_issue("- this is a test issue also (513)")
-    print parse_issue("  and this")
+    def test_in_progress_issue_with_number(self):
+        status, title, number = parse_issue("- this is a test issue also (513)")
+        self.assertEqual('in progress', status)
+        self.assertEqual('this is a test issue also', title)
+        self.assertEqual(513, number)
 
-if __name__ == '__main__':
-    test_issue_parsing()
+    def test_issue_no_number(self):
+        status, title, number = parse_issue("  and this")
+        self.assertEqual('pending', status)
+        self.assertEqual('and this', title)
+        self.assertIsNone(number)
+
+    def test_hanging_parenthesis(self):
+        _, title, number = parse_issue("  some title with a random extra (bracket")
+        self.assertEqual(title, "some title with a random extra (bracket")
+        self.assertIsNone(number)
+
